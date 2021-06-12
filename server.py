d654ac573b58448f35a435a936330f435a04995d
Sherwin Farrell Mascarenhas <smfarrell845@gmail.com>
2021-06-12 15:53:13 +0100

    Trying alternative for creating tables on heroku

--- server.py
+++ server.py
@@ -13,7 +13,6 @@ from shortener import Shortener
 app = Flask(__name__)
 
 ENV = 'prod'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 
 if ENV == 'dev':
     app.debug = True
@@ -70,6 +69,10 @@ def resolveUrl(code):
     else:
         print("Redirecting to url "+ resolved_url)
         return redirect(resolved_url)
+
+def create_tables():
+    with app.app_context():
+        db.create_all()
 
 if __name__ == '__main__':
     app.run('0.0.0.0',port=5000, threaded = True)
